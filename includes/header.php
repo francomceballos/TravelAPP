22b556acbd2278ef3d8e73fa3bd614e66c06f570
Franco Ceballos <francoceballos@skiff.com>
2024-06-03 10:32:36 -0300

    modified:   assets/css/templatemo-woox-travel.css 	new file:   auth/register.php 	new file:   includes/footer.php 	new file:   includes/header.php 	renamed:    index.html -> index.php 	deleted:    register.html

---
+++ includes/header.php
@@ -0,0 +1,80 @@
+<?php
+
+    define('BASE_URL', 'http://localhost/TravelAPP/');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>WoOx Travel Bootstrap 5 Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo BASE_URL; ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/templatemo-woox-travel.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/owl.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 580 Woox Travel
+
+https://templatemo.com/tm-580-woox-travel
+
+-->
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.php" class="active">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="deals.php">Deals</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>auth/login.php">Login</a></li>
+                        <li><a href="<?php echo BASE_URL; ?>auth/register.php">Register</a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
